80d8eec947e6e7109aefd89bd7ce060a2c310e57
Jenny Xu <jex004@ucsd.edu>
2025-01-24 15:14:44 -0800

    again

--- global.js
+++ global.js
@@ -18,13 +18,9 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  // Adjust URLS for Github
-  if (url.includes('github')) {
-    url = '/portfoilo/' + url;
-  }
 
   // Adjust URLs for non-home pages
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../portfolio/' + url : url;
 
   // Create the link
   let a = document.createElement('a');
